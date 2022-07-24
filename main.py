3dcaec76f48bf774ef2a55c846126f96d0592e6c
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2022-07-24 19:26:31 +0800

    connect lines in bottom

--- main.py
+++ main.py
@@ -71,10 +71,7 @@ def process_frame(frame):
         if pred[4].item() < 0.2:
             continue
 
-        center = (
-            round((pred[0].item() + pred[2].item()) / 2),
-            round((pred[1].item() + pred[3].item()) / 2),
-        )
+        center = (round((pred[0].item() + pred[2].item()) / 2), round(pred[3].item()))
 
         some_centers.append(center)
 
